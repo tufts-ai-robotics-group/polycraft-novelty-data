ac0184b149a09eb9c7243087a4e1c3e4e1f1e393
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-04-22 12:39:37 -0400

    In Module Data Downloading

---
+++ polycraft_nov_data/data.py
@@ -0,0 +1,40 @@
+import importlib.resources
+import os
+import urllib.request
+import zipfile
+
+
+DATA_LABELS = [
+    "normal",
+    "item",
+    "height",
+]
+
+DATA_URLS = {
+    "normal": "https://tufts.box.com/shared/static/7jrtn4tssu9palz3x13b6fq9jkleejbk.zip",
+    "item": "https://tufts.box.com/shared/static/p3kpy6njrlx6nvg3gh4t1klt5d3sjdp9.zip",
+    "height": "https://tufts.box.com/shared/static/3yfjmkm79yq2rl60kbhnq5eey530iqcv.zip",
+}
+
+DATA_FOLDERS = {
+    "normal": "normal",
+    "item": os.path.join("novel", "item"),
+    "height": os.path.join("novel", "height"),
+}
+
+with importlib.resources.path("polycraft_nov_data", "dataset") as dataset_root:
+    DATASET_ROOT = dataset_root
+
+DATA_PATHS = {key: os.path.join(DATASET_ROOT, val) for key, val in DATA_FOLDERS.items()}
+
+
+def download_datasets():
+    for label, data_path in DATA_PATHS.items():
+        zip_path = os.path.join(data_path, label + ".zip")
+        # assume data is downloaded if env_0 folder exists
+        if not os.path.isdir(os.path.join(data_path, "env_0")):
+            # download, extract, and delete zip of the data
+            urllib.request.urlretrieve(DATA_URLS[label], zip_path)
+            with zipfile.ZipFile(zip_path) as zip:
+                zip.extractall(data_path)
+            os.remove(zip_path)
