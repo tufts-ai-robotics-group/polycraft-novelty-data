1c3c4bc8c460eb1afb0241d209cf7b4e9d5563a3
Sarah Schneider <schneiders@C1SHP9901L>
2022-04-25 15:04:46 +0200

    Allow scaling for vgg16 preprocessing and use full (unpatched) images

--- polycraft_nov_data/image_transforms.py
+++ polycraft_nov_data/image_transforms.py
@@ -8,7 +8,6 @@ import polycraft_nov_data.data_const as data_const
 class ScaleImage:
     def __init__(self, image_scale=1.0):
         """Image scaling by multiplicative factor
-
         Args:
             image_scale (float, optional): Scaling to apply to image. Defaults to 1.0.
         """
@@ -24,10 +23,8 @@ class ScaleImage:
 class CropUI:
     def __call__(self, tensor):
         """Crop the UI by removing bottom row of patches
-
         Args:
             tensor (torch.tensor): Image tensor to remove Polycraft UI from
-
         Returns:
             torch.tensor: Cropped image tensor
         """
@@ -39,7 +36,6 @@ class CropUI:
 class SamplePatch:
     def __init__(self, patch_shape):
         """Sample a random patch from the set produced by ToPatches
-
         Args:
             patch_shape (tuple): Shape of patches to output
         """
@@ -47,10 +43,8 @@ class SamplePatch:
 
     def __call__(self, tensor):
         """Sample a random patch from the set produced by ToPatches
-
         Args:
             tensor (torch.tensor): Image tensor to sample a patch from
-
         Returns:
             torch.tensor: Patch image tensor
         """
@@ -62,7 +56,6 @@ class SamplePatch:
 class ToPatches:
     def __init__(self, patch_shape):
         """Divide image into set of patches
-
         Args:
             patch_shape (tuple): Shape of patches to output
         """
@@ -70,10 +63,8 @@ class ToPatches:
 
     def __call__(self, tensor):
         """Divide image into set of patches
-
         Args:
             tensor (torch.tensor): Image tensor to divide into patches
-
         Returns:
             torch.tensor: Set of patch image tensors, shape (PH, PW, C, H, W)
                           where PH and PW are number of patches vertically/horizontally
@@ -86,7 +77,6 @@ class ToPatches:
 class TrainPreprocess:
     def __init__(self, image_scale=1.0):
         """Image preprocessing for training
-
         Args:
             image_scale (float, optional): Scaling to apply to image. Defaults to 1.0.
         """
@@ -104,7 +94,6 @@ class TrainPreprocess:
 class TestPreprocess:
     def __init__(self, image_scale=1.0):
         """Image preprocessing for testing
-
         Args:
             image_scale (float, optional): Scaling to apply to image. Defaults to 1.0.
         """
@@ -124,7 +113,6 @@ class GaussianNoise:
     """
     def __init__(self, std=1/40):
         """Dataset transform to apply Gaussian Noise to normalized data
-
         Args:
             std (float, optional): STD of noise. Defaults to 1/40.
         """
@@ -142,11 +130,9 @@ class GaussianNoise:
 
 def patch_array_shape(patch_shape, tensor_shape):
     """Calculate shape of the output of ToPatches
-
     Args:
         patch_shape (tuple): Shape of patches to output
         tensor_shape (tuple): Shape of image tensor to divide into patches
-
     Returns:
         tuple: Shape of the output of ToPatches (PH, PW, C, H, W)
                where PH and PW are number of patches vertically/horizontally
@@ -158,9 +144,11 @@ def patch_array_shape(patch_shape, tensor_shape):
 class VGGPreprocess:
     """Preprocessing for VGG models
     """
-    def __init__(self) -> None:
+    def __init__(self, image_scale=1.0):
         self.preprocess = transforms.Compose([
             transforms.ToTensor(),
+            CropUI(),
+            ScaleImage(image_scale),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
