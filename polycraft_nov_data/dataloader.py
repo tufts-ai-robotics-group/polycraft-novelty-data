7328270cb2bd75b1deedf999fd9aaf95167ec578
Sarah Schneider <schneiders@C1SHP9901L>
2022-04-28 08:48:30 +0200

    include batch size to balanced sampling

--- polycraft_nov_data/dataloader.py
+++ polycraft_nov_data/dataloader.py
@@ -7,13 +7,13 @@ import polycraft_nov_data.dataset_transforms as dataset_transforms
 import polycraft_nov_data.image_transforms as image_transforms
 
 
-def balanced_sampler(train_set):
+def balanced_sampler(train_set, batch_size=32):
     # determine class balancing for training
     train_targets = torch.Tensor([target for _, target in train_set])
-    train_weight = torch.zeros_like(train_targets, dtype=float)
+    train_weight = torch.zeros_like(train_targets)#, dtype=float)
     for target in torch.unique(train_targets):
         train_weight[train_targets == target] = 1 / torch.sum(train_targets == target)
-    return data.WeightedRandomSampler(train_weight)
+    return data.WeightedRandomSampler(train_weight, batch_size)
 
 
 def polycraft_dataloaders(batch_size=32, image_scale=1.0, include_novel=False, shuffle=True):
@@ -90,19 +90,10 @@ def polycraft_dataloaders_full_image(batch_size=32, image_scale=1.0, include_nov
     class_splits = dataset_transforms.folder_name_to_target_key(dataset, class_splits)
     # split into datasets
     train_set, valid_set, test_set = dataset_transforms.filter_ep_split(dataset, class_splits)
-    # get DataLoaders for datasets
-    num_workers = 4
-    prefetch_factor = 1 if batch_size is None else max(batch_size//num_workers, 1)
-    dataloader_kwargs = {
-        "num_workers": num_workers,
-        "prefetch_factor": prefetch_factor,
-        "persistent_workers": True,
-        "batch_size": batch_size,
-        "shuffle": shuffle,
-    }
-    return (data.DataLoader(train_set, sampler=balanced_sampler(train_set), **dataloader_kwargs),
-            data.DataLoader(valid_set, **dataloader_kwargs),
-            data.DataLoader(test_set, **dataloader_kwargs))
+    
+    return (data.DataLoader(train_set, sampler=balanced_sampler(train_set, batch_size), batch_size=batch_size, shuffle=shuffle),
+            data.DataLoader(valid_set, batch_size=batch_size, shuffle=shuffle),
+            data.DataLoader(test_set, batch_size=batch_size, shuffle=shuffle))
 
 
 def polycraft_dataset_for_ms(batch_size=32, image_scale=1.0, patch_shape=(3, 32, 32),
