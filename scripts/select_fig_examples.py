9dbfa38ca1e1b421f307db058810e26293f749ca
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-06-06 10:33:22 -0400

    Select Figure Examples

---
+++ scripts/select_fig_examples.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import polycraft_nov_data.data_const as data_const
+
+
+if __name__ == "__main__":
+    targets_df = pd.read_csv(data_const.dataset_root / "targets.csv")
+    valid_eps = []
+    cur_ep = 0
+    cur_frames = 0
+    for _, row in targets_df.iterrows():
+        raw_path, nov_percent = row
+        nov_type, episode, frame = raw_path.split("/")
+        if episode != cur_ep:
+            cur_ep = episode
+            cur_frames = 0
+        if nov_percent >= data_const.NOV_THRESH and int(frame) in [1, 11, 21, 41]:
+            cur_frames += 1
+            if cur_frames == 3 and int(frame) >= 41:
+                valid_eps += [f"{nov_type}/{episode}"]
+    print(valid_eps)
